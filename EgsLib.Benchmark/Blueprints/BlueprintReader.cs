4c8b951e95f98deaefb5ea292a1848904e55f49a
NotOats <ian@aotc.us>
2023-11-01 19:45:13 -0400

    added initial blueprint benchmarking

---
+++ EgsLib.Benchmark/Blueprints/BlueprintReader.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using EgsLib.Blueprints;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace EgsLib.Benchmark.Blueprints
+{
+    //[MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net481)]
+    [SimpleJob(RuntimeMoniker.Net70)]
+    public class BlueprintReader
+    {
+        private static string ProgramFolder =>
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+            ?? throw new Exception("Failed to find current program folder");
+
+        private readonly List<string> _blueprintFiles;
+
+        public BlueprintReader()
+        {
+            _blueprintFiles = Directory
+                .EnumerateFiles(Path.Combine(ProgramFolder, @"Resources\Blueprints"), "*.epb")
+                .ToList();
+        }
+
+        [Benchmark]
+        public List<Blueprint> ReadBlueprints()
+        {
+            return _blueprintFiles
+                .Select(f => new Blueprint(f))
+                .ToList();
+        }
+    }
+}
---
+++ EgsLib.Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace EgsLib.Benchmark
+{
+    public class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
