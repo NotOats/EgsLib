d66e890b0b7bf5f5c202b84d977140b5d5d8950b
NotOats <ian@aotc.us>
2024-07-02 14:30:57 -0400

    added localization file reader and associated tests

---
+++ EgsLib.Tests/Localization/LocalizationFixture.cs
@@ -0,0 +1,12 @@
+﻿namespace EgsLib.Tests.Localization
+{
+    public class LocalizationFixture
+    {
+        public EgsLib.Localization Localization { get; }
+
+        public LocalizationFixture()
+        {
+            Localization = new EgsLib.Localization(@"Resources\Localization\Localization.csv");
+        }
+    }
+}
---
+++ EgsLib.Tests/Localization/LocalizationTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EgsLib;
+
+namespace EgsLib.Tests.Localization
+{
+    public class LocalizationTests : IClassFixture<LocalizationFixture>
+    {
+        private LocalizationFixture Fixture { get; }
+
+        public LocalizationTests(LocalizationFixture fixture)
+        {
+            Fixture = fixture;
+        }
+
+
+        [Theory]
+        [InlineData("English", "ConcreteDestroyedBlocks", "Concrete Blocks (Damaged)")]
+        [InlineData("English", "HullArmoredFull", "Hardened Steel Block")]
+        [InlineData("English", "bkiShieldCapacitor", "A secondary capacitor dedicated to supplying reserve energy to a shield generator; increasing shield strength at the cost of shield charge speed.")]
+        [InlineData("Deutsch", "ConcreteDestroyedBlocks", "Betonblöcke - zerstört")]
+        [InlineData("Français", "ConcreteDestroyedBlocks", "Blocs béton - détruits")]
+        public void TestReadValues(string language, string key, string expected)
+        {
+            var loc = Fixture.Localization;
+            var result = loc.Localize(key, language);
+
+            Assert.NotNull(result);
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("Deutsch", "PaxPurgatory")]
+        [InlineData("Deutsch", "MedicalIngredients")]
+        [InlineData("Deutsch", "ColonyLargeFood")]
+        public void TestReadEmptyValues(string language, string key)
+        {
+            var loc = Fixture.Localization;
+            var result = loc.Localize(key, language);
+
+            Assert.NotNull(result);
+            Assert.Equal(key, result);
+        }
+    }
+}
